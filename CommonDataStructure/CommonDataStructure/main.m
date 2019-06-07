c249f656af1682735ed87c2d7263635877fd8d07
janstilau <janstilau@gmail.com>
2019-06-07 16:02:42 +0800

    sync

---
+++ CommonDataStructure/CommonDataStructure/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CommonDataStructure
+//
+//  Created by JustinLau on 2019/3/25.
+//  Copyright © 2019 JustinLau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
